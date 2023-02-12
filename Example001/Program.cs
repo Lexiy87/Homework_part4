73b463c18da69e45cf0c785b0f79e8e47b2a8cae
Alexey_Volkov <alexiy87@inbox.ru>
2023-02-12 22:00:14 +0300

    feat:add new files for check homework

---
+++ Example001/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+double GetNumber (string message)
+{
+    double number =0;
+    System.Console.WriteLine(message);
+    
+    while (true)
+    {
+        if (double.TryParse(Console.ReadLine(), out number)&& number>0)
+        {
+            break;
+        }
+        else System.Console.WriteLine("Вы ввели неверное число, повторите ввод");
+    }
+    return number;
+}
+
+
+// System.Console.WriteLine($"{Math.Pow(number,degree)}");
+
+void DegreeOfNumber (double number, double degree)
+{
+    double result=0;
+    for (int i = 0; i <= degree; i++)
+    {
+        
+        result = Math.Pow(number,i);
+        
+
+    }
+    
+    System.Console.WriteLine(result);
+}
+
+double number = GetNumber ("Введите число");
+double degree = GetNumber ("Введите степень");
+
+DegreeOfNumber(number, degree);
---
+++ Example002/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+int GetNumber(string message)
+{
+    int number = 0;
+    System.Console.WriteLine(message);
+
+    while (true)
+    {
+        if (int.TryParse(Console.ReadLine(), out number))
+        {
+            break;
+        }
+        else System.Console.WriteLine("Неверное значение, повторите ввод");
+    }
+    return number;
+}
+
+
+int GetSumNumber(int number)
+{
+    int sum = 0;
+    while (number != 0)//10
+    {
+        sum = sum + number%10; //01
+        number = number / 10; //1
+                
+    }
+    return sum;
+}
+
+int number = GetNumber("Введите число");
+int sum = GetSumNumber(number);
+System.Console.WriteLine($"Сумма цифр в числе {sum}");
+
+
